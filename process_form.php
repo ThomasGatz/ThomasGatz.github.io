e7398139dda54658945c32c1631bdd4e2e91a410
Thomas Davis <tadavi24@asu.edu>
2023-10-30 21:09:56 -0700

    Commit 2

--- process_form.php
+++ process_form.php
@@ -1,4 +1,10 @@
 <?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
 $to = "tadavi24@asu.edu";
 $subject = "Form Submission";
 $message = "First Name: " . $_POST["fname"] . "\n";
